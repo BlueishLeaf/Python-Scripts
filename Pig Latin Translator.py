c33fd2170d27be94be2c082c7bc22ba4568a77fe
BlueishLeaf <killiank360@gmail.com>
2016-07-06 17:03:00 +0100

    Initial commit of pig latin translator and word reverser

---
+++ Pig Latin Translator.py
@@ -0,0 +1,4 @@
+original=input("Enter the word you want translated: ")
+print("Okay, converting "+original+" into pig latin...")
+translated=original[1:]+original[0]+"ay"
+print(original+" in pig latin is "+translated)
---
+++ Word Reverser.py
@@ -0,0 +1,4 @@
+original = input("Enter the word you would like to see backwards ")
+print("Okay, reversing " + original + "...")
+new = original[::-1]
+print(original + " reversed is " + new)
