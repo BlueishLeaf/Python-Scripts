ead38d2a9afa0fa9366ab06139b0c900456ecb78
Killian kelly <killiank360@gmail.com>
2016-09-22 21:19:54 +0100

    Fixed translator and deleted test

--- Pig Latin Translator.py
+++ Pig Latin Translator.py
@@ -1,4 +1,9 @@
-original=input("Enter the word you want translated: ")
-print("Okay, converting "+original+" into pig latin...")
-translated=original[1:]+original[0]+"ay"
-print(original+" in pig latin is "+translated)
+def piglatin ():
+    
+    original=input("Enter the word you want translated: ")
+    print("Okay, converting "+original+" into pig latin...")
+    translated=original[1:]+original[0]+"ay"
+    print(original+" in pig latin is "+translated)
+
+    piglatin()
+piglatin()
